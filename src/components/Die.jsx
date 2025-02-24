b9d519c58886db2acaffab4febe62ace5c3f2ee0
Rafif Ramdhana <rafiframadhana.cs@gmail.com>
2025-02-24 13:31:18 +0700

    Fix img address

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,13 +1,10 @@
-
-
 export default function Die(props) {
 
     return (
         <button className="dice-button" style={{backgroundColor: props.isHeld && '#59E391'}}
                 onClick={props.toggleIsHeld}
         >
-            {/* {props.value} */}
-            <img src={`src/assets/dice-${props.value}.png`} width="50" height="50"/>
+            <img src={`/tenzies-game/src/assets/dice-${props.value}.png`} width="50" height="50"/>
         </button>
     )
 }
